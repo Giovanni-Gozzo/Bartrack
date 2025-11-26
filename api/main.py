60d5cb67381478091a44fb0588eb76182a6ec455
Giovanni-Gozzo <gio.gozzo84@gmail.com>
2025-11-26 14:05:01 +0100

    first commit

---
+++ api/main.py
@@ -0,0 +1,16 @@
+import sys
+import uvicorn
+
+from fastapi import FastAPI
+
+
+
+
+@app.get("/")
+async def hello():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
